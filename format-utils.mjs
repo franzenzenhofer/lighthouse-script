f802d8902dcea0a99e0031695f6f011480d1d107
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2023-04-13 21:01:41 +0200

    update

--- format-utils.mjs
+++ format-utils.mjs
@@ -20,38 +20,51 @@ function getColor(value, goal, max) {
   return 'red';
 }
 
-export function formatAsHTML(results) {
+function formatCell(value, goal, max) {
+  const color = getColor(value, goal, max);
+  return `<td style="background-color: ${color}">${value}</td>`;
+}
+
+function formatRow(result) {
   const goalValues = {
     FCP: { goal: 1820, max: 2160 },
     SI: { goal: 3420, max: 4100 },
     LCP: { goal: 2520, max: 2950 },
     TBT: { goal: 200, max: 290 },
-    CLS: { goal: 0.1, max: 0.14 }
+    CLS: { goal: 0.1, max: 0.14 },
   };
 
-  const header = `<tr><th>URL</th><th>PSI</th><th>Performance</th><th>FCP</th><th>SI</th><th>LCP</th><th>TBT</th><th>CLS</th></tr>`;
-  const rows = results
-    .map(
-      ({
-        url,
-        performance,
-        firstContentfulPaint,
-        speedIndex,
-        largestContentfulPaint,
-        totalBlockingTime,
-        cumulativeLayoutShift
-      }) => {
-        const psiLink = `https://pagespeed.web.dev/analysis?url=${encodeURIComponent(url)}`;
-        const fcpColor = getColor(firstContentfulPaint, goalValues.FCP.goal, goalValues.FCP.max);
-        const siColor = getColor(speedIndex, goalValues.SI.goal, goalValues.SI.max);
-        const lcpColor = getColor(largestContentfulPaint, goalValues.LCP.goal, goalValues.LCP.max);
-        const tbtColor = getColor(totalBlockingTime, goalValues.TBT.goal, goalValues.TBT.max);
-        const clsColor = getColor(cumulativeLayoutShift, goalValues.CLS.goal, goalValues.CLS.max);
+  const {
+    url,
+    performance,
+    firstContentfulPaint,
+    speedIndex,
+    largestContentfulPaint,
+    totalBlockingTime,
+    cumulativeLayoutShift,
+    reportFilename,
+  } = result;
 
-        return `<tr><td><a href="${url}" target="_blank">${url}</a></td><td><a href="${psiLink}" target="_blank">PSI</a></td><td>${performance}</td><td style="background-color: ${fcpColor}">${firstContentfulPaint}</td><td style="background-color: ${siColor}">${speedIndex}</td><td style="background-color: ${lcpColor}">${largestContentfulPaint}</td><td style="background-color: ${tbtColor}">${totalBlockingTime}</td><td style="background-color: ${clsColor}">${cumulativeLayoutShift}</td></tr>`;
-      }
-    )
-    .join('');
+  const psiLink = `https://pagespeed.web.dev/analysis?url=${encodeURIComponent(url)}`;
+  const reportLink = `<a href="${reportFilename.replace('results', '..')}" target="_blank">HTML Report</a>`;
+
+  return `
+    <tr>
+      <td><a href="${url}" target="_blank">${url}</a></td>
+      <td><a href="${psiLink}" target="_blank">PSI</a></td>
+      <td>${performance}</td>
+      ${formatCell(firstContentfulPaint, goalValues.FCP.goal, goalValues.FCP.max)}
+      ${formatCell(speedIndex, goalValues.SI.goal, goalValues.SI.max)}
+      ${formatCell(largestContentfulPaint, goalValues.LCP.goal, goalValues.LCP.max)}
+      ${formatCell(totalBlockingTime, goalValues.TBT.goal, goalValues.TBT.max)}
+      ${formatCell(cumulativeLayoutShift, goalValues.CLS.goal, goalValues.CLS.max)}
+      <td>${reportLink}</td>
+    </tr>`;
+}
+
+export function formatAsHTML(results) {
+  const header = `<tr><th>URL</th><th>PSI</th><th>Performance</th><th>FCP</th><th>SI</th><th>LCP</th><th>TBT</th><th>CLS</th><th>HTML Report</th></tr>`;
+  const rows = results.map(formatRow).join('');
 
   return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Lighthouse Results</title></head><body><table>${header}${rows}</table></body></html>`;
 }
--- lighthouse-script.js
+++ lighthouse-script.js
@@ -1,5 +1,4 @@
 import fs from 'fs/promises';
-import { spawn } from 'child_process';
 import { performance } from 'perf_hooks';
 import lighthouse from 'lighthouse';
 import chromeLauncher from 'chrome-launcher';
@@ -10,24 +9,14 @@ const inputFile = 'urls.txt';
 const logDir = 'logs';
 const resultsDir = 'results';
 
-async function main() {
-  const urls = (await fs.readFile(inputFile, 'utf-8')).split('\n').filter(Boolean);
-  const ts = new Date().toISOString().replace(/[:.]/g, '-');
+async function processURLs(urls, logFile, ts, resultsSubDir) {
   const results = [];
-  const now = new Date();
-  const yearMonth = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}`;
-  const resultsSubDir = `${resultsDir}/${yearMonth}`;
-
-  await fs.mkdir(logDir, { recursive: true });
-  await fs.mkdir(resultsSubDir, { recursive: true });
-
-  const logFile = `${logDir}/lighthouse-script.log`;
 
   for (const url of urls) {
     console.log(`Running Lighthouse for ${url}`);
     await logToFile(logFile, `Running Lighthouse for ${url}`);
     const start = performance.now();
-    const r = await runLighthouse(url);
+    const r = await runLighthouse(url, ts, resultsSubDir);
     results.push(r);
     const end = performance.now();
     const d = (end - start) / 1000;
@@ -35,6 +24,10 @@ async function main() {
     await logToFile(logFile, `Lighthouse finished for ${url} in ${d.toFixed(2)} seconds`);
   }
 
+  return results;
+}
+
+async function saveResults(results, ts, resultsSubDir, logFile) {
   try {
     await writeFile(`${resultsSubDir}/lighthouse-results-${ts}.csv`, formatAsCSV(results));
     await writeFile(`${resultsSubDir}/lighthouse-results-${ts}.html`, formatAsHTML(results));
@@ -46,14 +39,43 @@ async function main() {
   }
 }
 
-async function runLighthouse(url) {
+async function main() {
+  const urls = (await fs.readFile(inputFile, 'utf-8')).split('\n').filter(Boolean);
+  const ts = new Date().toISOString().replace(/[:.]/g, '-');
+  const now = new Date();
+  const yearMonth = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}`;
+  const resultsSubDir = `${resultsDir}/${yearMonth}`;
+
+  await fs.mkdir(logDir, { recursive: true });
+  await fs.mkdir(resultsSubDir, { recursive: true });
+
+  const logFile = `${logDir}/lighthouse-script.log`;
+  const runSubDir = `${resultsSubDir}/${ts}`;
+  await fs.mkdir(runSubDir, { recursive: true });
+
+  const results = await processURLs(urls, logFile, ts, runSubDir);
+  await saveResults(results, ts, resultsSubDir, logFile);
+}
+
+async function runLighthouse(url, ts, runSubDir) {
   const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
-  const opts = { output: 'json', onlyCategories: ['performance'], port: chrome.port };
-  const res = await lighthouse(url, opts);
+  const opts = { output: ['json', 'html'], onlyCategories: ['performance'], port: chrome.port };
+  const results = await lighthouse(url, opts);
   await chrome.kill();
-  const { audits, categories } = res.lhr;
+  const { audits, categories } = results.lhr;
+
+  // Create a simple hash for the URL
+  const hash = url.split('').reduce((acc, char) => {
+    return (acc * 31 + char.charCodeAt(0)) & 0x7fffffff;
+  }, 0);
+
+  // Save the HTML report
+  const reportFilename = `${runSubDir}/report-${hash}-${ts}.html`;
+  await fs.writeFile(reportFilename, results.report[1]);
+
   return {
     url,
+    reportFilename,
     performance: categories.performance.score,
     firstContentfulPaint: audits['first-contentful-paint'].numericValue,
     speedIndex: audits['speed-index'].numericValue,
@@ -71,5 +93,6 @@ async function logToFile(file, message) {
 
 main().catch(async (e) => {
   console.error('Unexpected error:', e.message);
+  const logFile = `${logDir}/lighthouse-script.log`;
   await logToFile(logFile, `Unexpected error: ${e.message}`);
 });
